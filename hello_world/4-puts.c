6b074caeec97fd469a0bca06e9b89f743980fb65
root <raniaabassi33@gmail.com>
2023-06-16 02:29:09 -0700

    Hello, puts

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -3,13 +3,13 @@
 /**
  * main - Entry point
  *
- * Description:a C program that prints exactly "Programming is like building a multilingual puzzle
+ * Description:a C program that prints Programming is like building a multilingual puzzle
  *
  * Return: Always 0 (Success)
  *
  */
 int main(void)
 {
-puts('"Programming is like building a multilingual puzzle');
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
