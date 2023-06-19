a8a38af648f6116c41b709ca363c1057478f63e8
root <raniaabassi33@gmail.com>
2023-06-19 04:05:39 -0700

    positive or negative

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -2,8 +2,10 @@
 #include <time.h>
 #include <stdio.h>
 /** more headers goes there
-* main - Entry point 
+*main - Entry point
+*
 *Description:print negative or positive for the random number
+*
 * betty style doc for function main goes there
 */
 int main(void)
@@ -21,7 +23,7 @@ else if (n == 0)
 printf("%d is zero\n", n);
 }
 else
-{ 
+{
 printf("%d is negative\n", n);
 }
 return (0);
