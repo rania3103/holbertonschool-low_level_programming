9857321cb2abdca98c68fad95a56aa13ed4f3914
root <raniaabassi33@gmail.com>
2023-06-21 00:48:23 -0700

    print all single digit numbers of base 10

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Description: Prints last digit of a random number.
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+int i = 48;
+while (i <= 57)
+{
+putchar (i);
+i++;
+}
+putchar('\n');
+return (0);
+}
