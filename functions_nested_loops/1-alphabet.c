cd748748090ecb9ee7f1663fcf5538bf1fa670fc
root <raniaabassi33@gmail.com>
2023-06-21 05:45:11 -0700

    prints the alphabet, in lowercase

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+* main - Entry point
+*
+* Description: Prints last digit of a random number.
+*
+* Return: Always 0 (Success)
+*/
+void print_alphabet(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+	}
+	_putchar('\n');
+}
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,4 +1,5 @@
 #ifndef rania
 #define rania
 int _putchar(char c);
+void print_alphabet(void);
 #endif
