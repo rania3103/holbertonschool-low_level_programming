f47fb312b894c5c8a83534a8cbaed261009edd87
root <raniaabassi33@gmail.com>
2023-06-21 08:10:38 -0700

    print_alphabet_x10

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+/**
+* print_alphabet_x10 - Entry point
+*
+* Description: Prints 10 times the alphabet, in lowercase.
+*
+* Return: Always 0 (Success)
+*/
+void print_alphabet_x10(void)
+{
+int i = 0;
+char c = 'a';
+while (i < 10)
+{
+	while( c <= 'z')
+	{	
+		_putchar(c);
+		c++;
+	}
+	_putchar('\n');
+	i++;
+	c='a';
+}
+}
