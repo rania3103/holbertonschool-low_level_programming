92aab8af364601d0292d6ca7e060d4deaba07901
root <raniaabassi33@gmail.com>
2023-06-22 04:14:38 -0700

    print_last_digit

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,21 @@
+/**
+* print_last_digit - Entry point
+*
+* Description: prints the last digit of a number.
+*@n: number
+* Return: Always 0 (Success)
+*/
+int print_last_digit(int n)
+{
+	int x;
+
+	if (n < 0)
+	{
+		x = -1 * (n % 10);
+	}
+	else
+	{
+		x = n;
+	}
+	return (x);
+}
