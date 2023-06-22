08100b1c3e75558a60948deaa73b903c4701e079
root <raniaabassi33@gmail.com>
2023-06-22 05:41:04 -0700

    add 2 integers

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+* add - Entry point
+*
+* Description: adds two integers and returns the result.
+*@x :integer
+@y :integer
+* Return: Always 0 (Success)
+*/
+int add(int x, int y)
+{
+	return x + y;
+}
