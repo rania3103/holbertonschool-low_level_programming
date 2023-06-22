2ca5a893cbaea115101a8d4fe6f8b9303b5ef9b1
root <raniaabassi33@gmail.com>
2023-06-22 03:46:26 -0700

    checks for alphabetic character

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -2,13 +2,14 @@
 * _isalpha - Entry point
 *
 * Description: checks for alphabetic character.
-*c:character
+*c: character
 * Return: Always 0 (Success)
 */
 int _isalpha(int c)
 {
 	int test = 0;
-	if ( ( c >= 65 && c <= 90) || ( c >= 84 && c <= 122) )
+
+	if ((c >= 65 && c <= 90) || (c >= 84 && c <= 122))
 	{
 		test = 1;
 	}
