7bf2b72406e0159223f0c51871a5ef38427e2f84
root <raniaabassi33@gmail.com>
2023-06-23 03:34:58 -0700

    checks for uppercase character.

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+* _isupper - Entry point
+*
+* Description:checks for uppercase character.
+*@c:character
+* Return: Always 0 (Success)
+*/
+int _isupper(int c)
+{
+	int test = 0;
+
+	if(c >= 'A' && c <= 'Z')
+	{
+		test = 1;
+	}
+	return (test);
+}
