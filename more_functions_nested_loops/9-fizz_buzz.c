d09a34dc61e6874fe743ef6d119619e2578147dc
root <raniaabassi33@gmail.com>
2023-06-25 06:43:41 -0700

    Fizz-Buzz

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,59 @@
+#include "main.h"
+/**
+* main - Entry point
+*
+* Description: print numbers 1 to 100 and fizzbuzz for some conditions.
+*
+* Return: Always 0 (Success)
+*/
+int main()
+{
+        int i;
+
+        for (i = 1; i <= 100; i++)
+        {
+                if (i % 3 == 0)
+                {
+                        _putchar ('F');
+                        _putchar ('i');
+                        _putchar ('z');
+                        _putchar ('z');
+			_putchar(' ');
+                }
+		else if (i % 5 == 0)
+                {
+                        _putchar ('B');                       
+			_putchar ('u');
+                        _putchar ('z');
+			_putchar ('z');
+			_putchar(' ');
+		}
+		else if (i % 15 == 0)
+                {
+                        _putchar ('F');
+                        _putchar ('i');
+                        _putchar ('z');
+                        _putchar ('z');
+			_putchar ('B');
+                        _putchar ('u');
+                        _putchar ('z');
+                        _putchar ('z');
+			_putchar(' ');
+                }
+		else
+		{
+			if (i < 10)
+			{
+				_putchar (i + 48);
+				_putchar (' ');
+			}
+			else
+			{
+				_putchar (i / 10);
+				_putchar (i % 10);
+				_putchar (' ');
+			}
+		}
+	}
+return (0);
+}
