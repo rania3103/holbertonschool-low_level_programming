11f4d819ccc7289ad75ec9c15920ca3bd2c17de7
root <raniaabassi33@gmail.com>
2023-06-25 11:14:50 -0700

    print triangle

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -9,18 +9,24 @@
 void print_triangle(int size)
 {
 	int i, j, k;
-
-	for (i = 0; i < size; i++)
+	if (size <= 0)
 	{
-		for (j = 0; j < size - 1 - i; j++)
-		{
-			_putchar(' ');
-		}
-		for (k = 0; k <= i; k++)
-		{
-			_putchar('#');
-		}
 		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < size - 1 - i; j++)
+			{
+				_putchar(' ');
+			}
+			for (k = 0; k <= i; k++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
 	}
 
 }
