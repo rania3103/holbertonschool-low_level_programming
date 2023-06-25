c035f5a00c2f25d193c4f6f24758fc81f9202264
root <raniaabassi33@gmail.com>
2023-06-25 09:06:09 -0700

    fizz-buzz

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -8,53 +8,27 @@
 */
 int main(void)
 {
-        int i;
+int i;
 
-        for (i = 1; i <= 100; i++)
-        {
-                if (i % 3 == 0)
-                {
-                        putchar('F');
-                        putchar('i');
-                        putchar('z');
-                        putchar('z');
-			putchar(' ');
-                }
-		else if (i % 5 == 0)
-                {
-                        putchar('B');                       
-			putchar('u');
-                        putchar('z');
-			putchar('z');
-			putchar(' ');
-		}
-		else if (i % 15 == 0)
-                {
-                        putchar('F');
-                        putchar('i');
-                        putchar('z');
-                        putchar('z');
-			putchar('B');
-                        putchar('u');
-                        putchar('z');
-                        putchar('z');
-			putchar(' ');
-                }
-		else
-		{
-			if (i < 10)
-			{
-				putchar(i + 48);
-				putchar(' ');
-			}
-			else
-			{
-				putchar(i / 10 + 48);
-				putchar(i % 10 + 48);
-				putchar(' ');
-			}
-		}
+for (i = 1; i <= 100; i++)
+{
+	if (i % 3 == 0 && i % 5 == 0)
+	{
+		printf("FizzBuzz ");
 	}
-	putchar('\n');
-	return (0);
+	else if (i % 3 == 0)
+	{
+		printf("Fizz ");
+	}
+	else if (i % 5 == 0)
+	{
+		printf("Buzz ");
+	}
+	else
+	{
+		printf("%d ", i);
+	}
+}
+printf("\n");
+return (0);
 }
