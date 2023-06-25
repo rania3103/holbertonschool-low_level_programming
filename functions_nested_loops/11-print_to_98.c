49beebc45ef757f94409c3b5ac47bac6f63acb64
root <raniaabassi33@gmail.com>
2023-06-25 13:30:24 -0700

    print numbers to 98

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,45 @@
+#include "main.h"
+/**
+* print_to_98 - Entry point
+*
+* Description: prints all natural numbers from n to 98.
+*@n:integer
+* Return: Always 0 (Success)
+*/
+void print_to_98(int n)
+{
+	int i;
+
+	for (i = n; i <= 98; i++)
+	{
+		if (i < 10)
+		{
+			_putchar(i + 48);
+		}
+		else
+		{
+			_putchar(i / 10 + 48);
+			_putchar(i % 10 + 48);
+		}
+		if (i < 0)
+		{
+			if (i > -10)
+			{
+				_putchar('-');
+				_putchar((i * -1) % 10 + 48);
+			}
+			else
+			{
+				_putchar('-');
+				_putchar((i * -1) / 10 + 48);
+				_putchar((i * -1) % 10 + 48);
+			}
+		}
+		if (i != 98)
+		{
+			_putchar(',');
+		}
+		_putchar(' ');
+	}
+	_putchar('\n');
+}
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -10,4 +10,5 @@ int _abs(int);
 int print_last_digit(int);
 int add(int, int);
 void times_table(void);
+void print_to_98(int n);
 #endif
