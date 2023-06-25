ccc32602c297d432f7097ac86dbe9736a7589bfd
root <raniaabassi33@gmail.com>
2023-06-25 07:48:40 -0700

    fizz_buzz && git push

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -1,12 +1,12 @@
 #include "main.h"
 /**
-* main - Entry point
+*main -Entry-point
 *
 * Description: print numbers 1 to 100 and fizzbuzz for some conditions.
 *
 * Return: Always 0 (Success)
 */
-int main()
+int main(void)
 {
         int i;
 
@@ -14,46 +14,47 @@ int main()
         {
                 if (i % 3 == 0)
                 {
-                        _putchar ('F');
-                        _putchar ('i');
-                        _putchar ('z');
-                        _putchar ('z');
+                        _putchar('F');
+                        _putchar('i');
+                        _putchar('z');
+                        _putchar('z');
 			_putchar(' ');
                 }
 		else if (i % 5 == 0)
                 {
-                        _putchar ('B');                       
-			_putchar ('u');
-                        _putchar ('z');
-			_putchar ('z');
+                        _putchar('B');                       
+			_putchar('u');
+                        _putchar('z');
+			_putchar('z');
 			_putchar(' ');
 		}
 		else if (i % 15 == 0)
                 {
-                        _putchar ('F');
-                        _putchar ('i');
-                        _putchar ('z');
-                        _putchar ('z');
-			_putchar ('B');
-                        _putchar ('u');
-                        _putchar ('z');
-                        _putchar ('z');
+                        _putchar('F');
+                        _putchar('i');
+                        _putchar('z');
+                        _putchar('z');
+			_putchar('B');
+                        _putchar('u');
+                        _putchar('z');
+                        _putchar('z');
 			_putchar(' ');
                 }
 		else
 		{
 			if (i < 10)
 			{
-				_putchar (i + 48);
-				_putchar (' ');
+				_putchar(i + 48);
+				_putchar(' ');
 			}
 			else
 			{
-				_putchar (i / 10);
-				_putchar (i % 10);
-				_putchar (' ');
+				_putchar(i / 10 + 48);
+				_putchar(i % 10 + 48);
+				_putchar(' ');
 			}
 		}
 	}
-return (0);
+	_putchar('\n');
+	return (0);
 }
