7dbc2d6f2734a9282eca18aecc7a638df898fe32
root <raniaabassi33@gmail.com>
2023-06-27 00:13:26 -0700

    updates value of pointer to 98

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+* reset_to_98 - Entry point
+*
+* Description: updates the value of pointer to 98.
+*@n:integer
+* Return: Always 0 (Success)
+*/
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
