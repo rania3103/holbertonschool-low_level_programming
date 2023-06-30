a027e748c60c02a88e27e7de9526115112b18b57
rania3103 <raniaabassi33@gmail.com>
2023-06-30 10:01:54 +0100

    print a string in reverse

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -1,23 +1,24 @@
 #include "main.h"
 /**
-*print_rev- Entry point
-*
-*Description: prints a string in reverse.
-*@s:string
-*Return: Always 0 (Success)
+*  print_rev - Entry point
+* 
+* Description: prints a string in reverse.
+* @s: string
+* Return: void
 */
 void print_rev(char *s)
 {
-char* tmp = s;
-while (*s != '\0')
-{
-    s++;
-}
-s -= 1;
-while(s >= tmp)
-{
-    _putchar(*s);
-    s--;
-}
+    char* tmp = s;
+
+    while (*s != '\0')
+    {
+        s++;
+    }
+    s -= 1;
+    while (s >= tmp)
+    {
+        _putchar(*s);
+        s--;
+    }
 _putchar('\n');
 }
