0d6f8d4fc6644a2586edeec0a1ab2c4acde23530
rania3103 <raniaabassi33@gmail.com>
2023-07-01 12:25:09 +0100

    print array

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* print_array - Entry point
+* 
+* Description: print elements of an array.
+* @n: integer
+* @a:array
+* Return: void
+*/
+void print_array(int *a, int n)
+{
+    int* e = a + n;
+    int* b = a;
+
+    while (b < e)
+    {
+        if(b == e -1)
+        {
+            printf("%d",*b);
+        }
+        else
+        {
+            printf("%d, ",*b);
+        }
+        b++;
+    }
+    printf("\n");
+}
