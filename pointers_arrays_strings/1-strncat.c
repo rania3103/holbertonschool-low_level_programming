4e019001637db702e373c17f84f68aec2590566e
rania3103 <raniaabassi33@gmail.com>
2023-07-02 12:43:57 +0100

    concat 2 strings

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+* _strncat - Entry point
+* Description: concat 2 strings.
+* @dest: pointer
+* @src: pointer
+* @n: integer
+* Return: character
+*/
+char *_strncat(char *dest, char *src, int n)
+{
+    char* ptr = dest;
+
+    while (*dest != '\0')
+    {
+        dest++;
+    }
+    while (*src != '\0' && n > 0)
+    {
+        *dest = *src;
+        src++;
+        dest++;
+        n--;
+    }
+    *dest = '\0';
+    return (ptr);
+}
