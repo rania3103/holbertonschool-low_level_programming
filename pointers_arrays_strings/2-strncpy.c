29a83c4df54680bda0f64ab8ffe27ea22872df4d
rania3103 <raniaabassi33@gmail.com>
2023-07-02 12:54:35 +0100

    copy string

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+* _strncpy - Entry point
+* Description: copy string.
+* @dest: pointer
+* @src: pointer
+* @n: integer
+* Return: character
+*/
+char *_strncpy(char *dest, char *src, int n)
+{
+    char* ptr = dest;
+
+    while (*src != '\0' && n > 0)
+    {
+        *dest = *src;
+        src++;
+        dest++;
+        n--;
+    }
+    *dest = '\0';
+    return (ptr);
+}
