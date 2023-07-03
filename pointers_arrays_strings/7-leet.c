ec0e1229ce97acbd36941c6948a6e6b4827be64b
rania3103 <raniaabassi33@gmail.com>
2023-07-03 12:16:29 +0100

    encode a string into 1337

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <ctype.h>
+/**
+* leet - Entry point
+* Description: encodes a string into 1337.
+* @ch :pointer
+* Return: string
+*/
+char *leet(char *ch)
+{
+    char *ptr = ch;
+    char num[] = {'4','3','0','7','1'};
+    char c[]= {'a','e','o','t','l'};
+	int i = 0;
+
+    while (*ch != '\0')
+    {
+        while (i < 5)
+        {
+            if (toupper(*ch) == toupper(c[i]))
+            {
+                *ch = num[i];
+            }
+			i++;
+        }
+		ch++;
+    }
+    return (ptr);
+}
