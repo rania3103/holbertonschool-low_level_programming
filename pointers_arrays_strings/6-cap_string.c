d00db397f4b3151d88131e00a73f16c964b8529d
rania3103 <raniaabassi33@gmail.com>
2023-07-03 10:44:38 +0100

    capitalise string

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+* cap_string - Entry point
+* Description: capitalize all words of a string.
+* @ch :pointer
+* Return: string
+*/
+char *cap_string(char *ch)
+{
+	char *ptr = ch;
+
+	while (*ch != '\0')
+	{
+		if (*ch == ' ' || *ch == '"' || *ch == ',' || *ch == ';' || *ch == '?' || *ch =='!' || *ch =='{' || *ch =='}' || *ch =='(' || *ch ==')' || *ch == '.' || *ch == 9)
+		{
+			ch++;
+			if (*ch >= 'a' && *ch <= 'z')
+			{
+				*ch -= 32;
+			}
+		}
+		ch++;
+	}
+	return (ptr);
+}
