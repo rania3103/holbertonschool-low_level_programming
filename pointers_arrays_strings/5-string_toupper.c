d9cb22302bcecf889df656ada62b10a07f6633ad
rania3103 <raniaabassi33@gmail.com>
2023-07-03 10:00:31 +0100

    change string to uppercase

--- pointers_arrays_strings/5-string_toupper.c
+++ pointers_arrays_strings/5-string_toupper.c
@@ -2,12 +2,13 @@
 /**
 * string_toupper - Entry point
 * Description: change string char to uppercase.
+* @ch :pointer
 * Return: string
 */
 char *string_toupper(char *ch)
 {
 	char *ptr = ch;
-	
+
 	while (*ch != '\0')
 	{
 		if (*ch >= 'a' && *ch <= 'z')
