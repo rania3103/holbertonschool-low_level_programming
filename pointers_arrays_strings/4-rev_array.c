bc6a349ee146e5602c06896a31a6b77c807c7439
rania3103 <raniaabassi33@gmail.com>
2023-07-03 09:59:22 +0100

    reverse an array

--- pointers_arrays_strings/4-rev_array.c
+++ pointers_arrays_strings/4-rev_array.c
@@ -1,24 +1,25 @@
 #include "main.h"
 /**
 * reverse_array - Entry point
-* Description: concat 2 strings.
+* Description: reverse an array.
 * @a: pointer
 * @n: integer
 * Return: array
 */
 void reverse_array(int *a, int n)
 {
-    int* b = a;
-    int* e = a + (n - 1);
-    int num;
-    while (e > b)
-    {
-        num = *b;
-        *b = *e;
-        *e = num;
-        b++;
-        e--;
-    }
+	int *b = a;
+	int *e = a + (n - 1);
+	int num;
+
+	while (e > b)
+	{
+		num = *b;
+*b = *e;
+		*e = num;
+		b++;
+		e--;
+}
 }
 
 
