78a6bf0e53c632bacc64e526ca9a841057f2466c
rania3103 <raniaabassi33@gmail.com>
2023-07-04 11:16:32 +0100

    memset

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -3,20 +3,20 @@
 * _memset - Entry point
 * Description:fills memory with a constant byte.
 * @s :pointer
-* @ b :char
-* @ n:unsigned int
+* @b :char
+* @n:unsigned int
 * Return: integer
 */
 char *_memset(char *s, char b, unsigned int n)
 {
-    char *ptr = s;
-    unsigned int i = 0;
+	char *ptr = s;
+	unsigned int i = 0;
 
-    while (i < n)
-    {
-        *s = b;
-        s++;
-        i++;
-    }
-    return (ptr);
+	while (i < n)
+	{
+		*s = b;
+		s++;
+		i++;
+	}
+	return (ptr);
 }
