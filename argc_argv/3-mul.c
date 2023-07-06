278357203afa36b56599dbaa4954b26b7e11e330
rania3103 <raniaabassi33@gmail.com>
2023-07-06 13:43:20 +0100

    multiply 2 numbers

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+/**
+ * main -Entry point
+ * Description: multipy 2 numbers .
+ * @argc:argc
+ * @argv:argv
+ * Return: 0
+*/
+int main(int argc, char *argv[])
+{
+    int res;
+    if (argc != 3)
+    {
+        printf("%s\n","Error");
+        return (1);
+    }
+    res = (atoi(argv[1])) * (atoi(argv[2]));
+    printf("%d\n",res);
+	return (0);
+}
