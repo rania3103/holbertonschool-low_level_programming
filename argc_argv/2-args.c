0f9b27e32d8fdaf6143282b2d0027f901ce07966
rania3103 <raniaabassi33@gmail.com>
2023-07-06 13:13:26 +0100

    print all arguments

---
+++ argc_argv/2-args.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main -Entry point
+ * Description: prints all arguments .
+ * @argc:argc
+ * @argv:argv
+ * Return: 0
+*/
+int main(int argc, char *argv[])
+{
+	int i = 0;
+
+	while (i < argc)
+	{
+		printf("%s\n", argv[i]);
+		i++;
+	}
+	printf("\n");
+	return (0);
+}
