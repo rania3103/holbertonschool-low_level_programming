071c68ec776c218ee5c7d84130f1b1c0b05be57b
rania3103 <raniaabassi33@gmail.com>
2023-07-06 13:45:18 +0100

    multiply 2 numbers

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -10,13 +10,14 @@
 */
 int main(int argc, char *argv[])
 {
-    int res;
-    if (argc != 3)
-    {
-        printf("%s\n","Error");
-        return (1);
-    }
-    res = (atoi(argv[1])) * (atoi(argv[2]));
-    printf("%d\n",res);
+	int res;
+
+	if (argc != 3)
+	{
+		printf("%s\n", "Error");
+		return (1);
+	}
+	res = (atoi(argv[1])) * (atoi(argv[2]));
+	printf("%d\n", res);
 	return (0);
 }
