4c548a91ce13a303ce6b6585f834707bc6f6f9b3
rania3103 <raniaabassi33@gmail.com>
2023-07-06 08:56:12 +0100

    length of a string

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -3,12 +3,13 @@
 * _strlen_recursion - Entry point
 * Description: returns the length of a string.
 * @s :pointer
+* Return:integer
 */
 int _strlen_recursion(char *s)
 {
 	if (*s == '\0')
 	{
-		return 0;
+		return (0);
 	}
-	return (1 + (_strlen_recursion(s+1)));
+	return (1 + (_strlen_recursion(s + 1)));
 }
