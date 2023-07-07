f00920dd61f6e03385d2177971594617621199db
rania3103 <raniaabassi33@gmail.com>
2023-07-07 10:48:32 +0100

    concat 2 strings

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -1,7 +1,6 @@
 #include "main.h"
 /**
 * _strcat - Entry point
-* 
 * Description: concatenate 2 strings.
 * @dest: pointer
 * @src: pointer
@@ -9,18 +8,18 @@
 */
 char *_strcat(char *dest, char *src)
 {
-    char* ptr = dest;
+	char *ptr = dest;
 
-    while (*dest != '\0')
-    {
-        dest++;
-    }
-    while (*src != '\0')
-    {
-        *dest = *src;
-        src++;
-        dest++;
-    }
-    *dest = '\0';
-    return (ptr);
+	while (*dest != '\0')
+	{
+		dest++;
+	}
+	while (*src != '\0')
+	{
+		*dest = *src;
+		src++;
+		dest++;
+	}
+	*dest = '\0';
+	return (ptr);
 }
