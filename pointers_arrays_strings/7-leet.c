5f9b68123c2dbe5d33955eee643e0df96a6e773d
rania3103 <raniaabassi33@gmail.com>
2023-07-07 11:17:11 +0100

    leet

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -8,23 +8,23 @@
 */
 char *leet(char *ch)
 {
-    char *ptr = ch;
-    char num[] = {'4', '3', '0', '7', '1'};
-    char c[]= {'A', 'E' , 'O', 'T', 'L'};
+	char *ptr = ch;
+	char num[] = {'4', '3', '0', '7', '1'};
+	char c[] = {'A', 'E', 'O', 'T', 'L'};
 	int i = 0;
 
-    while (*ch != '\0')
-    {
+	while (*ch != '\0')
+	{
 		i = 0;
-        while (i < 5)
-        {
-            if (toupper(*ch) == c[i])
-            {
-                *ch = num[i];
-            }
+		while (i < 5)
+		{
+			if (toupper(*ch) == c[i])
+			{
+				*ch = num[i];
+			}
 			i++;
-        }
+		}
 		ch++;
-    }
-    return (ptr);
+	}
+	return (ptr);
 }
