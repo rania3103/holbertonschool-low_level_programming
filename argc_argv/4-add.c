dcad8a51f7eda080991e441998c2e9065bb9c6ee
rania3103 <raniaabassi33@gmail.com>
2023-07-07 09:51:17 +0100

    addition

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -12,28 +12,27 @@
 int main(int argc, char *argv[])
 {
 	int res = 0;
-    int i = 0;
-    int j =0;
+	int i = 0;
+	int j =0;
 
 	if (argc == 1)
 	{
 		printf("%d\n", 0);
 	}
-    while (i < argc)
-    {
-        while(argv[i][j] != '\0')
-        {
-            if (argv[i][j] < '0' && argv[i][j] > '9')
-            {
-                printf("%s\n","Error");
-                return (1);
-            }
-            j++;
-
-        }
-        res += atoi(argv[i]);
-        i++;  
-    }
-    printf("%d\n",res);
+	while (i < argc)
+	{
+		while(argv[i][j] != '\0')
+		{
+			if (argv[i][j] < '0' || argv[i][j] > '9')
+			{
+				printf("%s\n","Error");
+				return (1);
+			}
+			j++;
+		}
+		res += atoi(argv[i]);
+		i++;  
+	}
+	printf("%d\n",res);
 	return (0);
 }
