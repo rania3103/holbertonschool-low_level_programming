b666e283d297f6b41adecc401f090d954badbc26
rania3103 <raniaabassi33@gmail.com>
2023-07-07 12:00:35 +0100

    print chessboard

--- pointers_arrays_strings/7-print_chessboard.c
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -8,15 +8,17 @@
 void print_chessboard(char (*a)[8])
 {
 	int i = 0;
-	int j = 0;
+	int j;
 
 	while (i < 8)
 	{
+		j = 0;
 		while (j < 8)
 		{
 			_putchar(a[i][j]);
 			j++;
 		}
+		_putchar('\n');
 		i++;
 	}
 }
