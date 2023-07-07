a86f8df4c3cce79b5189f6e4d982782f02230536
rania3103 <raniaabassi33@gmail.com>
2023-07-07 11:30:47 +0100

    atoi

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,39 +1,38 @@
 #include "main.h"
 /**
 * _atoi - Entry point
-* 
 * Description: convert a string to an integer.
 * @s: pointer
 * Return: void
 */
 int _atoi(char *s)
 {
-    int sign = 1;
-    int res;
-    int num;
+	int sign = 1;
+	int res;
+	int num;
 
-    while(*s != '\0')
-    {
-        if(*s == ' ')
-        {
-            s++;
-            continue;
-        }
-        if(*s == '-')
-        {
-            s++;
-            sign = -1;
-        }
-        if(*s < '0' || *s > '9')
-        {
-            res = 0;
-        }
-        if (*s >= '0' && *s <= '9')
-        {
-            num = *s - 48;
-        }
-        res = res * 10 + num;
-        s++;
-    }
-    return (res *sign);
+	while (*s != '\0')
+	{
+		if (*s == ' ')
+		{
+			s++;
+			continue;
+		}
+		if (*s == '-')
+		{
+			s++;
+			sign = -1;
+		}
+		if (*s < '0' || *s > '9')
+		{
+			res = 0;
+		}
+		if (*s >= '0' && *s <= '9')
+		{
+			num = *s - 48;
+		}
+		res = res * 10 + num;
+		s++;
+	}
+	return (res * sign);
 }
