277b01796d9932f65dc4e1b97f285cb863e0221c
rania3103 <raniaabassi33@gmail.com>
2023-07-07 10:51:54 +0100

    copy string

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -9,20 +9,20 @@
 */
 char *_strncpy(char *dest, char *src, int n)
 {
-    char* ptr = dest;
+	char *ptr = dest;
 
-    while (*src != '\0' && n > 0)
-    {
-        *dest = *src;
-        src++;
-        dest++;
-        n--;
-    }
-    while (n > 0)
-    {
-        *dest = '\0';
-        dest++;
-        n--;
-    }
-    return (ptr);
+	while (*src != '\0' && n > 0)
+	{
+		*dest = *src;
+		src++;
+		dest++;
+		n--;
+	}
+	while (n > 0)
+	{
+		*dest = '\0';
+		dest++;
+		n--;
+	}
+	return (ptr);
 }
