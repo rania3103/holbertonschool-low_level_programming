20a4c6169a4d37a0435f049e346ce317971709fd
rania3103 <raniaabassi33@gmail.com>
2023-07-07 09:49:49 +0100

    addition

---
+++ argc_argv/4-add.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+#include <ctype.h>
+/**
+ * main -Entry point
+ * Description: add positive numbers .
+ * @argc:argc
+ * @argv:argv
+ * Return: 0
+*/
+int main(int argc, char *argv[])
+{
+	int res = 0;
+    int i = 0;
+    int j =0;
+
+	if (argc == 1)
+	{
+		printf("%d\n", 0);
+	}
+    while (i < argc)
+    {
+        while(argv[i][j] != '\0')
+        {
+            if (argv[i][j] < '0' && argv[i][j] > '9')
+            {
+                printf("%s\n","Error");
+                return (1);
+            }
+            j++;
+
+        }
+        res += atoi(argv[i]);
+        i++;  
+    }
+    printf("%d\n",res);
+	return (0);
+}
