b4c523cc5120b3618fe76e00e8a1eef01c50a295
rania3103 <raniaabassi33@gmail.com>
2023-07-07 10:08:31 +0100

    addition

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -25,7 +25,7 @@ int main(int argc, char *argv[])
 
 		while (argv[i][j] != '\0')
 		{
-			if (!(isdigit(argv[i][j])))
+			if (argv[i][j] < '0' || argv[i][j] > '9')
 			{
 				printf("%s\n", "Error");
 				return (1);
