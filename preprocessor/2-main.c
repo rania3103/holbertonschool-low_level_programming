993ba483e37f1b9134ebddcc012bdc83f50b62f5
rania3103 <raniaabassi33@gmail.com>
2023-07-14 11:30:25 +0100

    print the filename

---
+++ preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#define filename __FILE__
+/**
+* main - Entry point
+* Description: print the filename.
+* Return:0
+*/
+int main (void)
+{
+	printf("%s\n",filename);
+	return (0);
+}
