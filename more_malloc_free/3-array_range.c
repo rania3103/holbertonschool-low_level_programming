d1e23779c3076371b44c2ee8e0fd88145fefdb99
rania3103 <raniaabassi33@gmail.com>
2023-07-14 10:31:13 +0100

    create an array of integrs

---
+++ more_malloc_free/3-array_range.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+* array_range - Entry point
+* Description:creates an array of integers.
+* @min:int
+* @max:int
+* Return:pointer
+*/
+int *array_range(int min, int max)
+{
+	int *ptr;
+	int i;
+	int nb = 0;
+	int j = 0;
+
+	if (min > max)
+	{
+		return (NULL);
+	}
+	for(i = min;i <= max; i++)
+	{
+		nb+=1;
+	}
+	ptr = malloc(sizeof(int) * nb);
+	for(i = min;i <= max; i++)
+	{
+		ptr[j] = i;
+		j++;
+	}
+	return (ptr);
+}
