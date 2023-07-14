eaea685982f3a16d95751e32adb58edb08d26c4f
rania3103 <raniaabassi33@gmail.com>
2023-07-14 09:53:52 +0100

    allocate memory for an array

---
+++ more_malloc_free/2-calloc.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+/**
+* _calloc - Entry point
+* Description:allocates memory for an array using malloc.
+* @size:unsigned int
+* @nmemb:unsigned int
+* Return: void
+*/
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *ptr;
+
+	if (nmemb == 0 || size == 0)
+	{
+		return (NULL);
+	}
+	ptr = malloc(nmemb * size);
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+	return (ptr);
+	free(ptr);
+}
