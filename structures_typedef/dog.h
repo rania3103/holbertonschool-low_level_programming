23c00d90eec6b4254ded916cec4d6f42cc01b97d
rania3103 <raniaabassi33@gmail.com>
2023-07-17 11:07:18 +0100

    structures

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,10 +1,10 @@
 #ifndef do
 #define do
 /**
- * struct dog - struct 
- * @name: dog's name
- * @age : dog's age
- * @owner: owner of the dog
+* struct dog - struct
+* @name: dog's name
+* @age : dog's age
+* @owner: owner of the dog
 */
 struct dog
 {
