314d50e6cf6e2fa2dc345f282e2c98ec0434a398
rania3103 <raniaabassi33@gmail.com>
2023-07-20 11:10:34 +0100

    return the index of an element searched

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -15,11 +15,11 @@ int int_index(int *array, int size, int (*cmp)(int))
 	{
 		return (-1);
 	}
-	if (array)
+	if (array && cmp)
 	{
-		for (i = 0;i < size; i++)
+		for (i = 0; i < size; i++)
 		{
-			if(cmp(array[i]))
+			if (cmp(array[i]))
 			{
 				return (i);
 			}
