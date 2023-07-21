e2a9a9938e49905c1dfc372655423c150eb974e6
rania3103 <raniaabassi33@gmail.com>
2023-07-21 11:18:37 +0100

    print strings

---
+++ variadic_functions/2-print_strings.c
@@ -0,0 +1,37 @@
+#include "variadic_functions.h"
+/**
+ * print_strings- entry point
+ * Description: prints strings.
+ * @separator:character
+ * @n:unsigned integer
+*/
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list ptr;
+	unsigned int i;
+	char *str;
+
+	va_start(ptr, n);
+	if (n > 0)
+	{
+		for (i = 0; i < n - 1; i++)
+		{
+			str = va_arg(ptr, char *);
+			if (str == NULL)
+			{
+				printf("nil");
+			}
+			else
+			{
+				printf("%s%s", str, separator);
+			}
+		}
+		str = va_arg(ptr, char *);
+		printf("%s\n", str);
+	}
+	else
+	{
+		printf("\n");
+	}
+	va_end(ptr);
+}
