df03d064752d7bbfba99837c315b81a16448c090
rania3103 <raniaabassi33@gmail.com>
2023-07-21 10:58:00 +0100

    print numbers

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -11,13 +11,24 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 
 	va_start(ptr, n);
-	if (separator && n > 0)
+	if (n > 0)
 	{
 		for (i = 0; i < n - 1; i++)
 		{
-			printf("%d%s ", va_arg(ptr, int), separator);
+			if (separator == NULL)
+			{
+				printf("%d", va_arg(ptr, int));
+			}
+			else
+			{
+				printf("%d%s", va_arg(ptr, int), separator);
+			}
 		}
 		printf("%d\n", va_arg(ptr, int));
+	}
+	else
+	{
+		printf("\n");	
 	}
 	va_end(ptr);
 }
