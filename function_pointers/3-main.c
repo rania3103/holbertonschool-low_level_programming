e2297009279e94a28c17d56c465b3256015c1c91
rania3103 <raniaabassi33@gmail.com>
2023-07-25 17:20:46 +0100

    calculator with simple operations

---
+++ function_pointers/3-main.c
@@ -0,0 +1,38 @@
+#include "3-calc.h"
+/**
+ * main - entry point
+ * @argc:argument count
+ * @argv:argument vector
+ * Return: 0 (success)
+*/
+int main(int argc, char *argv[])
+{
+	int num1;
+	int num2;
+	char *op;
+	int (*func_ptr)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+	op = argv[2];
+	func_ptr = get_op_func(op);
+
+	if (func_ptr == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	if ((*op == '/' || *op == '%') && num2 == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	printf("%d\n", func_ptr(num1, num2));
+return (0);
+}
---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,60 @@
+#include "3-calc.h"
+/**
+ * op_add - entry point
+ * Descriprion: add 2 integers
+ * @a:integer
+ * @b:integer
+ * Return: result of type integer
+*/
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - entry point
+ * Descriprion: subtract 2 integers
+ * @a:integer
+ * @b:integer
+ * Return: result of type integer
+*/
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - entry point
+ * Descriprion: multipy 2 integers
+ * @a:integer
+ * @b:integer
+ * Return: result of type integer
+*/
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - entry point
+ * Descriprion: divide 2 integers
+ * @a:integer
+ * @b:integer
+ * Return: result of type integer
+*/
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - entry point
+ * Descriprion: modulo
+ * @a:integer
+ * @b:integer
+* Return: result of type integer
+*/
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
