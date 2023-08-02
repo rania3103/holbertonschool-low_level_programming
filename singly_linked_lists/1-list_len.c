6ad90296b0f0b1d4f07c88de9528a8f7a634bbbf
rania3103 <raniaabassi33@gmail.com>
2023-08-02 09:43:59 +0100

    print elemnts of list

---
+++ singly_linked_lists/1-list_len.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+/**
+ *list_len - prints the number of elements in a linked list
+ *@h:pointer
+ *Return: the number of elements
+*/
+size_t list_len(const list_t *h)
+{
+	const list_t *current;
+	int nb;
+
+	current = h;
+	nb = 0;
+
+	while (current != NULL)
+	{
+		if (current->str != NULL && current->len != 0)
+		{
+			nb++;
+		}
+		current = current->next;
+	}
+	return (nb);
+}
+
