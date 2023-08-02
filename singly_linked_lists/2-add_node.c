541980eb61d2c687bac2e4855dfb23459e8167b6
rania3103 <raniaabassi33@gmail.com>
2023-08-02 11:24:24 +0100

    check node

---
+++ singly_linked_lists/2-add_node.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+/**
+ *add_node - adds a new node at the beginning of a list
+ *@head:head of the list
+ *@str:string
+ *Return:  the address of the new element, or NULL if it failed
+*/
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new;
+
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+	{
+		return (NULL);
+	}
+	new->str = strdup(str);
+	new->len = strlen(new->str);
+	new->next = *head;
+	*head = new;
+	return (new);
+}
+
