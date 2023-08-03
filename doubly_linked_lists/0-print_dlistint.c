d87b821cc353bedf317434c29e69b15ed516eeda
rania3103 <raniaabassi33@gmail.com>
2023-08-03 09:25:32 +0100

    check code

---
+++ doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ * print_dlistint -  prints all the elements of a dlistint_t list
+ * @h:pointer
+ * Return:the number of nodes
+*/
+size_t print_dlistint(const dlistint_t *h)
+{
+	const dlistint_t *ptr;
+	int nb;
+
+	ptr = h;
+	nb = 0;
+	while (ptr != NULL)
+	{
+		printf("%d\n",ptr->n);
+		ptr = ptr->next;
+		nb++;
+	}
+	return  (nb);
+}
