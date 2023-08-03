2b1db334dc21fd8f47338d18a7d8da8d3c8e41eb
rania3103 <raniaabassi33@gmail.com>
2023-08-03 08:59:11 +0100

    check code

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ *free_list - frees list
+ *@h:pointer
+*/
+void free_list(list_t *head)
+{
+	list_t *ptr;
+	ptr = head;
+
+	while (ptr != NULL)
+	{
+		free(ptr->str);
+		free(ptr);
+		ptr = ptr->next;
+	}
+}
+
