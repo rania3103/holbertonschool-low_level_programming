f62e383c5e1fba2423485e2fc1c4f2f96947c0de
rania3103 <raniaabassi33@gmail.com>
2023-08-07 07:34:30 +0100

    check code

---
+++ bit_manipulation/1-print_binary.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_binary - prints the binary representation of a number
+ * @n: The number to convert to binary and print.
+*/
+void print_binary(unsigned long int n)
+{
+	int shift = sizeof(unsigned long int) * 8 - 1;
+	int bit = 1;
+
+	while (shift >= 0)
+	{
+		bit = (n >> shift) & 1;
+		_putchar(bit + 48);
+		shift--;
+	}
+}
