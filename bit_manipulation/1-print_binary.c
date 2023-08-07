c72dcb4b89131eff306efdc3d065fb9245b3f9bf
rania3103 <raniaabassi33@gmail.com>
2023-08-07 07:41:24 +0100

    fix error

--- bit_manipulation/1-print_binary.c
+++ bit_manipulation/1-print_binary.c
@@ -6,12 +6,12 @@
 void print_binary(unsigned long int n)
 {
 	int shift = sizeof(unsigned long int) * 8 - 1;
-	int bit = 1;
+	int bit = 0;
 
 	while (shift >= 0)
 	{
 		bit = (n >> shift) & 1;
-		_putchar(bit + 48);
+		_putchar(bit + '0');
 		shift--;
 	}
 }
