e82d88b0731812126d3362a05ba6882cb737e495
rania3103 <raniaabassi33@gmail.com>
2023-08-08 12:25:48 +0100

    check code

---
+++ file_io/1-create_file.c
@@ -0,0 +1,37 @@
+#include "main.h"
+/**
+ * create_file - creates a file.
+ * @filename:the name of the file to create.
+ * @text_content:is a NULL terminated string to write to the file.
+ * Return:1 on success, -1 on failure.
+*/
+int create_file(const char *filename, char *text_content)
+{
+	int fdesc, i, w;
+
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+	fdesc = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 600);
+	if (fdesc == -1)
+	{
+		return (-1);
+	}
+	i = 0;
+	if (text_content != NULL)
+	{
+		while (text_content[i] != '\0')
+		{
+			i++;
+		}
+		w = write(fdesc, text_content, i);
+		if (w == -1)
+		{
+			close(fdesc);
+			return (-1);
+		}
+	}
+	close(fdesc);
+	return (1);
+}
