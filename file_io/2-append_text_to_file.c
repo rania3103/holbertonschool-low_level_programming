79089a12ab9569d34206b6b5e0b68544cbfbd5c5
rania3103 <raniaabassi33@gmail.com>
2023-08-09 08:31:39 +0100

    fix error

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -18,12 +18,12 @@ int append_text_to_file(const char *filename, char *text_content)
 	{
 		return (-1);
 	}
-	if (text_content != NULL && fdesc != -1)
+	if (text_content != NULL)
 	{
 		w = write(fdesc, text_content, strlen(text_content));
 		if (w == -1)
 		{
-			return (-1);
+			return (1);
 		}
 	}
 	close(fdesc);
