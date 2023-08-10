944cbde9554f8ed5db025519309fe40dde4531fa
rania3103 <raniaabassi33@gmail.com>
2023-08-10 08:28:46 +0100

    check code

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -26,37 +26,31 @@ int main(int argc, char *argv[])
 	file_to = argv[2];
 	file_from = argv[1];
 	if (argc != 3)
-	{
 		print_message("Usage: cp file_from file_to\n", "", 97);
-	}
 	fdesc1 = open(file_from, O_RDONLY);
 	if (fdesc1 == -1)
-	{
 		print_message("Error: Can't read from file %s\n", file_from, 98);
-	}
 	fdesc2 = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, 0644);
 	if (fdesc2 == -1)
-	{
 		print_message("Error: Can't write to %s\n", file_to, 99);
-	}
 	if (container != NULL)
 	{
 		r = read(fdesc1, container, sizeof(container));
 		if (r == -1)
-		{
 			print_message("Error: Can't read from file %s\n", file_from, 98);
-		}
 		w = write(fdesc2, container, r);
 		if (w == -1)
-		{
 			print_message("Error: Can't write to %s\n", file_to, 99);
-		}
 	}
 	if (close(fdesc1) == -1)
+	{
 		dprintf(2, "Error: Can't close fd %d\n", fdesc1);
 		exit(100);
+	}
 	if (close(fdesc2) == -1)
+	{
 		dprintf(2, "Error: Can't close fd %d\n", fdesc2);
 		exit(100);
+	}
 	return (0);
 }
