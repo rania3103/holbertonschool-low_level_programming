5ffccde91a345957e8072ac1f36823b92f1e8f10
rania3103 <raniaabassi33@gmail.com>
2023-09-20 10:21:41 +0100

    add prototyê

--- search_algorithms/search_algos.h
+++ search_algorithms/search_algos.h
@@ -3,4 +3,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 int linear_search(int *array, size_t size, int value);
+void print_subarray(int *array, size_t b, size_t end);
+int binary_search(int *array, size_t size, int value);
 #endif
